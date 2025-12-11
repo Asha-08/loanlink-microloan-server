f3bdd1354df784bc88d3a064e77672b6b1d12135
Asha-08 <ashaamina91@gmail.com>
2025-12-11 21:12:10 +0600

    add get loanId api

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/loans/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await loansCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/loans',async(req,res)=>{
         const loan = req.body;
         const result = await loansCollection.insertOne(loan);
