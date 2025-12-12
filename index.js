308afa98557abbf11ae0ef21426725ca5b887544
Asha-08 <ashaamina91@gmail.com>
2025-12-13 02:11:00 +0600

    update user role

--- index.js
+++ index.js
@@ -8,9 +8,35 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const port = process.env.PORT || 3000;
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./loanlink-microloan-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 // middleware
 app.use(express.json());
 app.use(cors());
+
+const verifyFBToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send({ message: "unathorized access" });
+  }
+
+  try {
+    const idToken = token.split(" ")[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    console.log("decoded in the token", decoded);
+    req.decoded_email = decoded.email;
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: "unathurized access" });
+  }
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.us9qyhi.mongodb.net/?appName=Cluster0`;
 
@@ -31,6 +57,92 @@ async function run() {
     const db = client.db("loan_link_db");
     const loansCollection = db.collection("loans");
     const paymentCollection = db.collection("payments");
+    const userCollection = db.collection("users");
+
+    // user related api
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "user";
+      user.requestedRole = user.requestedRole || null;
+      user.status = "pending";
+      user.createdAt = new Date();
+      const email = user.email;
+      const userExist = await userCollection.findOne({ email });
+
+      if (userExist) {
+        return res.send({
+          message: "user exist",
+          existing: true,
+          user: userExist,
+        });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send({
+        existing: false,
+        user: result,
+      });
+    });
+
+    // for social login user api
+    app.patch("/users/request-role/:email", async (req, res) => {
+      const email = req.params.email;
+      const { requestedRole } = req.body;
+
+      const result = await userCollection.updateOne(
+        { email },
+        { $set: { requestedRole } }
+      );
+
+      res.send(result);
+    });
+
+    // update user role
+
+    app.patch("/users/:id", verifyFBToken, async (req, res) => {
+      const status = req.body.status;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const user = await userCollection.findOne(query);
+      if (!user) return res.status(404).send({ message: "User not found" });
+
+      const updatedDoc = {
+        $set: {
+          status: status,
+          role: user.requestedRole || user.role,
+          requestedRole: null,
+        },
+      };
+      const result = await userCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    // suspend user api
+
+    app.patch("/users/suspend/:id", async (req, res) => {
+      const id = req.params.id;
+      const { reason, feedback } = req.body;
+
+      const result = await userCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $set: {
+            status: "suspended",
+            suspendReason: reason,
+            suspendFeedback: feedback,
+          },
+        }
+      );
+
+      res.send(result);
+    });
 
     //  loans api
     app.get("/loans", async (req, res) => {
@@ -196,12 +308,20 @@ async function run() {
     });
 
     //  payment related apis
-    app.get("/payments", async (req, res) => {
+    app.get("/payments", verifyFBToken, async (req, res) => {
       const email = req.query.email;
       const loanId = req.query.loanId;
       const query = {};
+
+      // console.log('headers',req.headers);
+
       if (email) {
         query.customerEmail = email;
+
+        // check email address
+        if (email !== req.decoded_email) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
       }
 
       if (loanId) {
